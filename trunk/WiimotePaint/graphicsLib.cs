230fa5e7ea144f2ea83f733c566100612cfc333c
bmwesting <bmwesting@ad3fb3fa-6d45-0410-ad30-ed870affe951>
2008-02-12 20:32:17 +0000

    -Added scale factor for wiimote cursor

--- trunk/WiimotePaint/graphicsLib.cs
+++ trunk/WiimotePaint/graphicsLib.cs
@@ -7,10 +7,14 @@ using WiimoteLib;
 
 namespace PaintProgram
 {
+
     class graphicsLib
     {
-        int x_dim=640, y_dim=480;
-        float scale_x, scale_y;
+        const int WIIMOTE_RAW_X = 1023; //values reported according to Wiili
+        const int WIIMOTE_RAW_Y = 767;
+        int x_dim=640, y_dim=480;       //dimensions of image
+        float scale_x=640/WIIMOTE_RAW_X, scale_y=480/WIIMOTE_RAW_Y;
+
         Bitmap b = new Bitmap(640, 480, PixelFormat.Format24bppRgb);
         Graphics g;
 
@@ -24,6 +28,8 @@ namespace PaintProgram
             b = new Bitmap(x,y, PixelFormat.Format24bppRgb);
             x_dim = x;
             y_dim = y;
+            x_scale = x / WIIMOTE_RAW_X;
+            y_scale = y / WIIMOTE_RAW_Y;
         }
 
         public void graphicsLib(int x, int y, Bitmap b)
@@ -36,15 +42,15 @@ namespace PaintProgram
         public Bitmap drawCursorPoints(WiimoteState ws)
         {
             if (ws.IRState.Found1)
-                g.DrawEllipse(new Pen(Color.Red), (int)(ws.IRState.RawX1 / 4), (int)(ws.IRState.RawY1 / 4), ws.IRState.Size1 + 1, ws.IRState.Size1 + 1);
+                g.DrawEllipse(new Pen(Color.Red), (int)(ws.IRState.RawX1*scale_x), (int)(ws.IRState.RawY1*scale_y), ws.IRState.Size1 + 1, ws.IRState.Size1 + 1);
             if (ws.IRState.Found2)
-                g.DrawEllipse(new Pen(Color.Blue), (int)(ws.IRState.RawX2 / 4), (int)(ws.IRState.RawY2 / 4), ws.IRState.Size2 + 1, ws.IRState.Size2 + 1);
+                g.DrawEllipse(new Pen(Color.Blue), (int)(ws.IRState.RawX2 * scale_x), (int)(ws.IRState.RawY2 * scale_y), ws.IRState.Size2 + 1, ws.IRState.Size2 + 1);
             if (ws.IRState.Found3)
-                g.DrawEllipse(new Pen(Color.Yellow), (int)(ws.IRState.RawX3 / 4), (int)(ws.IRState.RawY3 / 4), ws.IRState.Size3 + 1, ws.IRState.Size3 + 1);
+                g.DrawEllipse(new Pen(Color.Yellow), (int)(ws.IRState.RawX3 * scale_x), (int)(ws.IRState.RawY3 * scale_y), ws.IRState.Size3 + 1, ws.IRState.Size3 + 1);
             if (ws.IRState.Found4)
-                g.DrawEllipse(new Pen(Color.Orange), (int)(ws.IRState.RawX4 / 4), (int)(ws.IRState.RawY4 / 4), ws.IRState.Size4 + 1, ws.IRState.Size4 + 1);
+                g.DrawEllipse(new Pen(Color.Orange), (int)(ws.IRState.RawX4 * scale_x), (int)(ws.IRState.RawY4 * scale_y), ws.IRState.Size4 + 1, ws.IRState.Size4 + 1);
             if (ws.IRState.Found1 && ws.IRState.Found2)
-                g.DrawEllipse(new Pen(Color.Green), (int)(ws.IRState.RawMidX / 4), (int)(ws.IRState.RawMidY / 4), 2, 2);
+                g.DrawEllipse(new Pen(Color.Green), (int)(ws.IRState.RawMidX * scale_x), (int)(ws.IRState.RawMidY * scale_y), 2, 2);
         }        
 
     }
